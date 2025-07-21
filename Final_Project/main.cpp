b4fad154c4069ee77e992c30e8d339c4bcf69040
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 20:21:29 +0330

    main.cpp
    ویرایش فایل main و قرار دادن ادمین اولیه در لیست ادمین ها. و همچنین فراخوانی صفحه ورود

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,11 +1,30 @@
+#include<ctime>
 #include "mainwindow.h"
+
+#include"LinkedList.h"
+#include"User.h"
+#include"Account.h"
+
+#include"Admin.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+
+    srand(time(0));
+
     QApplication a(argc, argv);
-    MainWindow w;
+
+
+    LinkedList<User> allUsers;
+    LinkedList<Account> allAccounts;
+    std::shared_ptr<Admin> firstAdmin = std::make_shared<Admin>("علی", "عزیزی", "1234567890", 20, "admin1", "1234");
+
+    allUsers.listPushBack(firstAdmin);
+
+
+    MainWindow w(allUsers, allAccounts);
     w.show();
     return a.exec();
 }
