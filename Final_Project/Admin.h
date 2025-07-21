0d649620909111dc89bb869efe8305e9c0f27794
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 13:27:00 +0330

    ویژگی های Admin
    متدها و توابع کلاس Admin اعلان شدند

--- Final_Project/Admin.h
+++ Final_Project/Admin.h
@@ -1,4 +1,11 @@
 #ifndef ADMIN_H
 #define ADMIN_H
+#include"User.h"
+
+class Admin: public User
+{
+public:
+    Admin(const QString&, const QString&, const QString&, int, const QString&, const QString&);
+};
 
 #endif // ADMIN_H
