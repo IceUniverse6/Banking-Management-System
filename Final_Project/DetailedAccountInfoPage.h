16031ab6ba4355f8b5a460e35c924a34ff3db510
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 15:06:37 +0330

    DetailedAccountInfoPage.h
    متدهای این صفحه اعلان شدند

--- Final_Project/DetailedAccountInfoPage.h
+++ Final_Project/DetailedAccountInfoPage.h
@@ -1,6 +1,8 @@
 #ifndef DETAILEDACCOUNTINFOPAGE_H
 #define DETAILEDACCOUNTINFOPAGE_H
 
+#include"Account.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,8 +14,11 @@ class DetailedAccountInfoPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit DetailedAccountInfoPage(QWidget *parent = nullptr);
+    explicit DetailedAccountInfoPage(std::shared_ptr<Account>, QWidget *parent = nullptr);
     ~DetailedAccountInfoPage();
+
+private slots:
+    void on_okButton_clicked();
 
 private:
     Ui::DetailedAccountInfoPage *ui;
