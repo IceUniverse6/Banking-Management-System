f4b10dce97b1a2eb336c3a9198b0c124c29893b6
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 14:55:49 +0330

    PersonalInfoPage.cpp
    فایل cpp مربوط به صفحه مشاهده اطلاعات شخصی پیاده سازی شد

--- Final_Project/PersonalInfoPage.cpp
+++ Final_Project/PersonalInfoPage.cpp
@@ -1,14 +1,28 @@
 #include "PersonalInfoPage.h"
 #include "ui_PersonalInfoPage.h"
 
-PersonalInfoPage::PersonalInfoPage(QWidget *parent)
+PersonalInfoPage::PersonalInfoPage(std::shared_ptr<User> user,QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::PersonalInfoPage)
 {
     ui->setupUi(this);
+    this->setWindowTitle("اطلاعات شخصی");
+
+    ui->firstName->setText(user->getFirstName());
+    ui->lastName->setText(user->getLastName());
+    ui->userName->setText(user->getUserName());
+    ui->nationalId->setText(user->getNationalId());
+    ui->age->setText(QString::number(user->getAge()));
 }
 
 PersonalInfoPage::~PersonalInfoPage()
 {
     delete ui;
 }
+
+void PersonalInfoPage::on_okButton_clicked()
+{
+    this->close();
+    return;
+}
+
