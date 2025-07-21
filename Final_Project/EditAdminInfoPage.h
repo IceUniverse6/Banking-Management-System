6f9be12a0f96da4c40d41ca41035a090df936d7c
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 18:27:49 +0330

    EditAdminInfoPage.h
    ویژگی ها و توابع مورد نیاز این صفحه اعلان شدند

--- Final_Project/EditAdminInfoPage.h
+++ Final_Project/EditAdminInfoPage.h
@@ -1,6 +1,8 @@
 #ifndef EDITADMININFOPAGE_H
 #define EDITADMININFOPAGE_H
 
+#include"User.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +14,16 @@ class EditAdminInfoPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit EditAdminInfoPage(QWidget *parent = nullptr);
+    explicit EditAdminInfoPage(const LinkedList<User>&, std::shared_ptr<User>, QWidget *parent = nullptr);
     ~EditAdminInfoPage();
+
+private slots:
+    void on_editInfoButton_clicked();
 
 private:
     Ui::EditAdminInfoPage *ui;
+    std::shared_ptr<User> adminUser;
+    const LinkedList<User>& allUsers;
 };
 
 #endif // EDITADMININFOPAGE_H
