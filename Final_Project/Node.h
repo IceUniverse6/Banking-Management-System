963598c8bf619cfb3e6743d3695abbd24838b76b
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 10:03:59 +0330

    پیاده سازی کلاس Node
    کلاس Node پیاده سازی شد

--- Final_Project/Node.h
+++ Final_Project/Node.h
@@ -1,4 +1,37 @@
 #ifndef NODE_H
 #define NODE_H
+#include<memory>
+
+template<class T>
+class Node{
+
+private:
+    std::shared_ptr<T> data;
+    std::shared_ptr<Node<T>> next;
+public:
+    Node()
+        :data(nullptr), next(nullptr)
+    {}
+    Node(const std::shared_ptr<T>& data)
+        :data(data), next(nullptr)
+    {}
+
+    ~Node()=default;
+
+    void setData(std::shared_ptr<T> data){
+        this->data=data;
+    }
+    void setNext(std::shared_ptr<Node<T>> next){
+        this->next=next;
+    }
+
+    const std::shared_ptr<T>& getData()const {
+        return this->data;
+    }
+    const std::shared_ptr<Node<T>>& getNext()const {
+        return this->next;
+    }
+
+};
 
 #endif // NODE_H
