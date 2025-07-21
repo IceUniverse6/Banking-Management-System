dedc19057af0fe132a11a1609e7a027d15e2d422
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 20:01:44 +0330

    CostumerPanel.h
    اعلان ویژگی ها و توابع مورد نیاز برای عملکرد  پنل مشتری

--- Final_Project/CostumerPanel.h
+++ Final_Project/CostumerPanel.h
@@ -1,6 +1,8 @@
 #ifndef COSTUMERPANEL_H
 #define COSTUMERPANEL_H
 
+#include"Costumer.h"
+
 #include <QMainWindow>
 
 namespace Ui {
@@ -12,11 +14,32 @@ class CostumerPanel : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit CostumerPanel(QWidget *parent = nullptr);
+    explicit CostumerPanel(std::shared_ptr<Costumer>, LinkedList<User>&, LinkedList<Account>&, QWidget *parent = nullptr);
     ~CostumerPanel();
+
+private slots:
+    void on_viewPersonalInfoButton_clicked();
+
+    void on_viewAccountsButton_clicked();
+
+    void on_signOutButton_clicked();
+
+    void on_changeAccountPassButton_clicked();
+
+    void on_changefirstPassButton_clicked();
+
+    void on_changeStaticSecondPassButton_clicked();
+
+    void on_getBalanceButton_clicked();
+
+    void on_transferMoneyButton_clicked();
 
 private:
     Ui::CostumerPanel *ui;
+
+    std::shared_ptr<Costumer> costumerUser;
+    LinkedList<User>& allUsers;
+    LinkedList<Account>& allAccounts;
 };
 
 #endif // COSTUMERPANEL_H
