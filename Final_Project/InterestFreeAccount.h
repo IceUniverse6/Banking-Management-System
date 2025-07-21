04882f832a475efc0a3e3da0b67bbb1d01c81594
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 14:35:47 +0330

    ویژگی های حساب قرض الحسنه
    متدهای کلاس حساب قرض الحسنه اعلان شدند

--- Final_Project/InterestFreeAccount.h
+++ Final_Project/InterestFreeAccount.h
@@ -1,4 +1,14 @@
 #ifndef INTERESTFREEACCOUNT_H
 #define INTERESTFREEACCOUNT_H
+#include"Account.h"
+
+class InterestFreeAccount: public Account
+{
+public:
+    InterestFreeAccount(const std::shared_ptr<Costumer>&, const QString&, const QString&, const QString&, const QString&, const QDate&, const QString&, const QString&, double);
+
+
+    QString getAccountType()const;
+};
 
 #endif // INTERESTFREEACCOUNT_H
