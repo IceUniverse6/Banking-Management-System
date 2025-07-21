eb5e4f99860cca6aa997c8b54dd9b66c1b8b8066
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 19:15:56 +0330

    ViewAccountsForBalance.h
    اعلان توابع و ویژگی های مورد نیاز برای عملکرد این صفحه

--- Final_Project/ViewAccountsForBalance.h
+++ Final_Project/ViewAccountsForBalance.h
@@ -1,6 +1,8 @@
 #ifndef VIEWACCOUNTSFORBALANCE_H
 #define VIEWACCOUNTSFORBALANCE_H
 
+#include"Account.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +14,18 @@ class ViewAccountsForBalance : public QDialog
     Q_OBJECT
 
 public:
-    explicit ViewAccountsForBalance(QWidget *parent = nullptr);
+    explicit ViewAccountsForBalance(const LinkedList<Account>&, QWidget *parent = nullptr);
     ~ViewAccountsForBalance();
+
+private slots:
+    void on_okButton_clicked();
+
+    void on_returnButton_clicked();
 
 private:
     Ui::ViewAccountsForBalance *ui;
+
+    const LinkedList<Account>& accounts;
 };
 
 #endif // VIEWACCOUNTSFORBALANCE_H
