53651e7aae5e08818ee9bb5d2dda4f2f7b20e313
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 17:25:37 +0330

    CreateNewBankAccountPage.h
    توابع مورد نیاز برای این صفحه اعلان شدند

--- Final_Project/CreateNewBankAccountPage.h
+++ Final_Project/CreateNewBankAccountPage.h
@@ -1,6 +1,9 @@
 #ifndef CREATENEWBANKACCOUNTPAGE_H
 #define CREATENEWBANKACCOUNTPAGE_H
 
+#include"User.h"
+#include"Account.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +15,16 @@ class CreateNewBankAccountPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit CreateNewBankAccountPage(QWidget *parent = nullptr);
+    explicit CreateNewBankAccountPage(const LinkedList<User>&, LinkedList<Account>&, QWidget *parent = nullptr);
     ~CreateNewBankAccountPage();
+
+private slots:
+    void on_createAccountButton_clicked();
 
 private:
     Ui::CreateNewBankAccountPage *ui;
+    const LinkedList<User>& allUsers;
+    LinkedList<Account>& allAccounts;
 };
 
 #endif // CREATENEWBANKACCOUNTPAGE_H
