ac35780fd7851b7fc70121449a01955113b9340f
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 16:37:36 +0330

    ViewAllAccountsPage.h
    متدهای مورد نیاز عملکرد این صفحه اعلان شدند

--- Final_Project/ViewAllAccountsPage.h
+++ Final_Project/ViewAllAccountsPage.h
@@ -1,6 +1,8 @@
 #ifndef VIEWALLACCOUNTSPAGE_H
 #define VIEWALLACCOUNTSPAGE_H
 
+#include"Account.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +14,17 @@ class ViewAllAccountsPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit ViewAllAccountsPage(QWidget *parent = nullptr);
+    explicit ViewAllAccountsPage(const LinkedList<Account>&, QWidget *parent = nullptr);
     ~ViewAllAccountsPage();
+
+private slots:
+    void on_searchButton_clicked();
+
+    void on_returnButton_clicked();
 
 private:
     Ui::ViewAllAccountsPage *ui;
+    const LinkedList<Account>& allAccounts;
 };
 
 #endif // VIEWALLACCOUNTSPAGE_H
