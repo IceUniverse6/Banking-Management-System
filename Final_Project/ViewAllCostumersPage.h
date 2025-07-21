e94a2bfc7afaa64c2a662e16b40015de973484c0
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 16:21:03 +0330

    ViewAllCostumersPage.h
    متدهای این صفحه اعلان شدند

--- Final_Project/ViewAllCostumersPage.h
+++ Final_Project/ViewAllCostumersPage.h
@@ -1,6 +1,8 @@
 #ifndef VIEWALLCOSTUMERSPAGE_H
 #define VIEWALLCOSTUMERSPAGE_H
 
+#include"User.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +14,17 @@ class ViewAllCostumersPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit ViewAllCostumersPage(QWidget *parent = nullptr);
+    explicit ViewAllCostumersPage(const LinkedList<User>&, QWidget *parent = nullptr);
     ~ViewAllCostumersPage();
+
+private slots:
+    void on_searchButton_clicked();
+
+    void on_returnButton_clicked();
 
 private:
     Ui::ViewAllCostumersPage *ui;
+    const LinkedList<User>& allUsers;
 };
 
 #endif // VIEWALLCOSTUMERSPAGE_H
