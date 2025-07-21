f70a48d49fd685182a0d9ab7e28b64035ce36e35
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 17:15:22 +0330

    AccountInfoPage.h
    متدهای مورد نیاز عملکرد این صفحه اعلان شدند

--- Final_Project/AccountInfoPage.h
+++ Final_Project/AccountInfoPage.h
@@ -1,6 +1,7 @@
 #ifndef ACCOUNTINFOPAGE_H
 #define ACCOUNTINFOPAGE_H
 
+#include"Account.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,8 +13,11 @@ class AccountInfoPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit AccountInfoPage(QWidget *parent = nullptr);
+    explicit AccountInfoPage(const std::shared_ptr<Account>, QWidget *parent = nullptr);
     ~AccountInfoPage();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::AccountInfoPage *ui;
