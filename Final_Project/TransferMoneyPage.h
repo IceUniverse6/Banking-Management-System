7688e981c198ef7f8de62c671d194e772838c580
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 19:28:13 +0330

    TransferMoneyPage.h
    اعلان ویژگی ها و توابع مورد نیاز صفحه انتقال وجه در هدر فایل

--- Final_Project/TransferMoneyPage.h
+++ Final_Project/TransferMoneyPage.h
@@ -1,7 +1,10 @@
 #ifndef TRANSFERMONEYPAGE_H
 #define TRANSFERMONEYPAGE_H
 
+#include"Account.h"
+
 #include <QDialog>
+#include<QDate>
 
 namespace Ui {
 class TransferMoneyPage;
@@ -12,11 +15,22 @@ class TransferMoneyPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit TransferMoneyPage(QWidget *parent = nullptr);
+    explicit TransferMoneyPage(const QString&, std::shared_ptr<Account>, std::shared_ptr<Account>, QWidget *parent = nullptr);
     ~TransferMoneyPage();
+
+
+private slots:
+    void on_returnButton_clicked();
+
+    void on_generateDynamicSecondPassButton_clicked();
+
+    void on_transferButton_clicked();
 
 private:
     Ui::TransferMoneyPage *ui;
+    std::shared_ptr<Account> fromAccount;
+    std::shared_ptr<Account> toAccount;
+
 };
 
 #endif // TRANSFERMONEYPAGE_H
