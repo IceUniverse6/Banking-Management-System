61b8ab5668c76ca1cc76a60859ad16b0a1fe5372
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 14:21:26 +0330

    ویژگی های حساب جاری
    متدهای کلاس حساب جاری اعلان شدند

--- Final_Project/CurrentAccount.h
+++ Final_Project/CurrentAccount.h
@@ -1,4 +1,13 @@
 #ifndef CURRENTACCOUNT_H
 #define CURRENTACCOUNT_H
+#include"Account.h"
+
+class CurrentAccount:public Account
+{
+public:
+    CurrentAccount(const std::shared_ptr<Costumer>&, const QString&, const QString&, const QString&, const QString&, const QDate&, const QString&, const QString&, double);
+
+    QString getAccountType()const;
+};
 
 #endif // CURRENTACCOUNT_H
