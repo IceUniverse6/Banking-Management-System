7156e231a790e2a6a2c658ddcfcc7ddb2fe759b0
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 17:16:56 +0330

    AccountInfoPage.cpp
    متدهای مورد نیاز برای عملکرد این صفحه پیاده سازی شدند

--- Final_Project/AccountInfoPage.cpp
+++ Final_Project/AccountInfoPage.cpp
@@ -1,14 +1,30 @@
 #include "AccountInfoPage.h"
 #include "ui_AccountInfoPage.h"
 
-AccountInfoPage::AccountInfoPage(QWidget *parent)
+AccountInfoPage::AccountInfoPage(const std::shared_ptr<Account> account, QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::AccountInfoPage)
 {
     ui->setupUi(this);
+
+    this->setWindowTitle("اطلاعات حساب");
+
+    ui->accountType->setText(account->getAccountType());
+    ui->shebaNumber->setText(account->getShebaNumber());
+    ui->cardNumber->setText(account->getCardNumber());
+    ui->owner->setText(account->getOwnerName());
+    ui->expirationDate->setText(account->getExpirationDate().toString("MM/yy"));
+    ui->cvv2->setText(account->getCvv2());
 }
 
 AccountInfoPage::~AccountInfoPage()
 {
     delete ui;
 }
+
+void AccountInfoPage::on_pushButton_clicked()
+{
+    this->close();
+    return;
+}
+
