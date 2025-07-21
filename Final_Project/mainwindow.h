221d5dd5097990b8be60bf6c3995ab3bdc1d69d2
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 20:18:06 +0330

    MainWindow.h
    اعلان ویژگی ها و توابع مورد نیاز برای عملکرد صفحه ورود

--- Final_Project/mainwindow.h
+++ Final_Project/mainwindow.h
@@ -1,6 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include"User.h"
+#include"Account.h"
+
+#include"AdminPanel.h"
+#include"CostumerPanel.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -14,10 +20,18 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(LinkedList<User>&, LinkedList<Account>&, QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_enterButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    LinkedList<User>& allUsers;
+    LinkedList<Account>& allAccounts;
+    std::shared_ptr<AdminPanel> adminPanel;
+    std::shared_ptr<CostumerPanel> costumerPanel;
 };
 #endif // MAINWINDOW_H
