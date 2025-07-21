e24ade31c11a359da5d4ec97fb0de2eaca7463f1
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 17:53:36 +0330

    RemoveAccountPage.h
    اعلان ویژگی ها و متدهای مورد نیاز برای عملکرد این صفحه

--- Final_Project/RemoveAccountPage.h
+++ Final_Project/RemoveAccountPage.h
@@ -1,5 +1,6 @@
 #ifndef REMOVEACCOUNTPAGE_H
 #define REMOVEACCOUNTPAGE_H
+#include"Account.h"
 
 #include <QDialog>
 
@@ -12,11 +13,15 @@ class RemoveAccountPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit RemoveAccountPage(QWidget *parent = nullptr);
+    explicit RemoveAccountPage(LinkedList<Account>&, QWidget *parent = nullptr);
     ~RemoveAccountPage();
+
+private slots:
+    void on_removeAccountButton_clicked();
 
 private:
     Ui::RemoveAccountPage *ui;
+    LinkedList<Account>& allAccounts;
 };
 
 #endif // REMOVEACCOUNTPAGE_H
