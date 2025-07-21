8608c443aa64213af3d20c9d1dcada5982ff65e1
IceUniverse6 <ali84azizi2005@gmail.com>
2025-07-21 16:29:55 +0330

    ViewAllAdminsPage.h
    متدها و توابع مورد نیاز برای عملکرد این صفحه اعلان شدند

--- Final_Project/ViewAllAdminsPage.h
+++ Final_Project/ViewAllAdminsPage.h
@@ -1,6 +1,7 @@
 #ifndef VIEWALLADMINSPAGE_H
 #define VIEWALLADMINSPAGE_H
 
+#include"User.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,8 +13,11 @@ class ViewAllAdminsPage : public QDialog
     Q_OBJECT
 
 public:
-    explicit ViewAllAdminsPage(QWidget *parent = nullptr);
+    explicit ViewAllAdminsPage(const LinkedList<User>&, QWidget *parent = nullptr);
     ~ViewAllAdminsPage();
+
+private slots:
+    void on_okButton_clicked();
 
 private:
     Ui::ViewAllAdminsPage *ui;
